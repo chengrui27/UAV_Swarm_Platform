7ae65a81f4dccbae96c42718983267ad8a84c26e
chengrui27 <m13328628031@163.com>
2025-04-09 13:57:05 +0800

    使用yolov5 + deep_sort_realtime进行多目标跟踪

--- src/yolov5_ros/scripts/detect_ros.py
+++ src/yolov5_ros/scripts/detect_ros.py
@@ -5,14 +5,18 @@ import rospy
 import cv2
 import torch
 import numpy as np
+import onnxruntime as ort
 import warnings
 from functools import partial
 from cv_bridge import CvBridge, CvBridgeError
+
 
 from std_msgs.msg import Header
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image, RegionOfInterest
 from yolov5_ros.msg import BoundingBox, BoundingBoxes
+from deep_sort_realtime.deepsort_tracker import DeepSort
+from threadpoolctl import threadpool_limits
 
 class Yolov5Param:
     def __init__(self):
@@ -32,6 +36,25 @@ class Yolov5Param:
         else:
             self.model.cuda()
         self.model.conf = conf
+
+        # 初始化DeepSort
+        self.tracker = DeepSort(
+                 max_age=15,  # 最多允许丢失目标的帧数，超出就会删除该检测框以及相应的轨迹
+                 max_iou_distance=1.0,  # 预测框和目标框的最大距离，超过该距离则判断丢失目标，该参数越高越不容易丢失目标，但是会容易导致误匹配
+                 max_cosine_distance=0.2, # 行人重识别（ReID）的最大余弦距离，超过该阈值则判断丢失目标，该参数越高越不容易丢失目标，但是稳定性会降低
+                 n_init=3,  # 连续检测到一定次数，才确认生成检测框和对应的轨迹
+                 embedder="mobilenet",  # 使用的模型，mobilenet是适用于无人机等移动机器人的轻量化模型
+                 half=True,  # 使用半精度浮点数（FP16）加速推理，需要使用GPU
+                 bgr=True,  # 输入图像是否采用BGR格式，该格式是OpenCV的默认格式
+                 embedder_gpu=True,  # 是否使用GPU
+                 nn_budget=100  # 限制历史样本数量，加快匹配速度
+        )
+
+        # 打印GPU和CUDA的使用情况，检查yolov5和deepsort是否使用gpu加速
+        print("Available providers:", ort.get_available_providers())
+        print("CUDA Available:", torch.cuda.is_available())
+        if torch.cuda.is_available():
+           print("Using GPU:", torch.cuda.get_device_name(0))
 
         # target publishers
         # BoundingBoxes是本样例自定义的消息类型，用来记录识别到的目标
@@ -54,40 +77,60 @@ def image_cb(msg, cv_bridge, yolov5_param, color_classes, image_pub):
     # 将BGR图像转换为RGB图像, 给yolov5，其返回识别到的目标信息
     rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = yolov5_param.model(rgb_image)
-    boxs = results.pandas().xyxy[0].values
+
+    # 使用pandas读取yolov5返回的识别结果，简单易用，但是运算速度慢
+    # boxs = results.pandas().xyxy[0].values
+    # detections = []
+    # boxs = [box for box in boxs if str(box[-1]) == 'person']
+    # for box in boxs:
+    #     x1, y1, x2, y2, conf, cls_name = box[0], box[1], box[2], box[3], box[4], box[-1]
+    #     w, h = x2 - x1, y2 - y1
+    #     detections.append(([x1, y1, w, h], conf, cls_name))  # DeepSORT 接收 xywh 格式
+
+    # 直接用numpy进行数据的读取效率更高
+    boxs = results.xyxy[0].cpu().numpy()  
+    detections = []
     for box in boxs:
+        x1, y1, x2, y2, conf, cls_id = box[:6]
+        cls_name = yolov5_param.model.names[int(cls_id)]
+
+        # 只识别处理person，剔除其他物体，提高检测和跟踪的运行速度
+        if cls_name != 'person':
+            continue
+
+        w, h = x2 - x1, y2 - y1
+        detections.append(([x1, y1, w, h], conf, cls_name))  # DeepSORT 接收 xywh 格式
+
+    # 限制update时只使用一个线程，防止CPU占用率过高，影响GPU加速
+    with threadpool_limits(limits=1, user_api='blas'):
+        tracks = yolov5_param.tracker.update_tracks(detections, frame=frame)
+
+    for track in tracks:
+        if not track.is_confirmed():
+            continue
+        track_id = track.track_id
+        ltrb = track.to_ltrb()  # (x1, y1, x2, y2)
+        cls_name = track.get_det_class()  # 需要 deep_sort_realtime >= 1.3
+
+        color = color_classes.get(cls_name, np.random.randint(0, 255, 3))
+        color_classes[cls_name] = color
+
+        # 画框 + ID
+        cv2.rectangle(cv_image, (int(ltrb[0]), int(ltrb[1])), (int(ltrb[2]), int(ltrb[3])),
+                      (int(color[0]), int(color[1]), int(color[2])), 2)
+        cv2.putText(cv_image, f'{cls_name} ID:{track_id}', (int(ltrb[0]), int(ltrb[1]) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+        # 处理并存储识别 + 跟踪结果
         bounding_box = BoundingBox()
-        # 置信度，因为是基于统计，因此每个目标都有一个置信度，标识可能性
-        bounding_box.probability =np.float64(box[4])
-        # （xmin, ymin）是目标的左上角，（xmax,ymax）是目标的右上角
-        bounding_box.xmin = np.int64(box[0])
-        bounding_box.ymin = np.int64(box[1])
-        bounding_box.xmax = np.int64(box[2])
-        bounding_box.ymax = np.int64(box[3])
-        # 本地识别到的目标个数
-        bounding_box.num = np.int16(len(boxs))
-        # box[-1]是目标的类型名，比如person
-        bounding_box.Class = box[-1]
-        
-        # 放入box队列中
-        bounding_boxes.bounding_boxes.append(bounding_box)
-        # 同一类目标，用同一个颜色的线条画框
-        if box[-1] in color_classes.keys():
-            color = color_classes[box[-1]]
-        else:
-            color = np.random.randint(0, 183, 3)
-            color_classes[box[-1]] = color
-    
-        # 用框把目标圈出来
-        cv2.rectangle(cv_image, (int(box[0]), int(box[1])),
-                        (int(box[2]), int(box[3])), (int(color[0]),int(color[1]), int(color[2])), 2)    
-		# 在框上, 打印物体类型信息Class
-        if box[1] < 20:
-            text_pos_y = box[1] + 30
-        else:
-            text_pos_y = box[1] - 10    
-        cv2.putText(cv_image, box[-1],
-                    (int(box[0]), int(text_pos_y)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2, cv2.LINE_AA)    
+        bounding_box.probability = 1.0  # track没有置信度，设为1
+        bounding_box.xmin = int(ltrb[0])
+        bounding_box.ymin = int(ltrb[1])
+        bounding_box.xmax = int(ltrb[2])
+        bounding_box.ymax = int(ltrb[3])
+        bounding_box.num = len(tracks)
+        bounding_box.Class = cls_name
+        bounding_boxes.bounding_boxes.append(bounding_box)   
 
     # 发布目标数据，topic为：/yolov5/targets
     # 可以使用命令查看：rotopic echo /yolov5/targets
